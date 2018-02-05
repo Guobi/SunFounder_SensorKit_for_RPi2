a1e3ea209cd45c694217e6dd98d1b5ee0eefb135
Guobi Wu <guobi.wu@gmail.com>
2018-02-05 23:59:41 +0800

    update

--- Python/01_traffic_light.py
+++ Python/01_traffic_light.py
@@ -2,12 +2,17 @@
 import RPi.GPIO as GPIO
 import time
 
-colors = [0xFF00, 0x00FF, 0xFFFF, 0x1FFF, 0x0FFF]
+dictColor = {
+	'red': 0xFF00, 
+	'green': 0x00FF, 
+	'yellow': 0x0FFF,
+	'black': 0x0000}
+
 pins = (11, 12)  # pins is a dict
 
 GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-GPIO.setup(pins, GPIO.OUT)   # Set pins' mode is output
-GPIO.output(pins, GPIO.LOW)  # Set pins to LOW(0V) to off led
+GPIO.setup(pins, GPIO.OUT)     # Set pins' mode is output
+GPIO.output(pins, GPIO.LOW)    # Set pins to LOW(0V) to off led
 
 p_R = GPIO.PWM(pins[0], 2000)  # set Frequece to 2KHz
 p_G = GPIO.PWM(pins[1], 2000)
@@ -24,17 +29,31 @@ def setColor(col):   # For example : col = 0x1122
 	
 	R_val = map(R_val, 0, 255, 0, 100)
 	G_val = map(G_val, 0, 255, 0, 100)
-	print('R: ', R_val)
-	print('G: ', G_val)
+	# print('R: ', R_val)
+	# print('G: ', G_val)
 	p_R.ChangeDutyCycle(R_val)     # Change duty cycle
 	p_G.ChangeDutyCycle(G_val)
 
+def flashColor(col, count):
+	print("Flash {} {} times".format(col, count))
+	for i in range(1, count+1):
+		setColor(dictColor[col])
+		time.sleep(0.5)
+		setColor(dictColor['black'])
+		time.sleep(0.2)
+	return
+
+def displayColor(col, seconds):
+	print("Display {} for {} seconds".format(col, seconds))
+	setColor(dictColor[col])
+	time.sleep(seconds)
+	return
+
 def loop():
 	while True:
-		for col in colors:
-			# print(col)
-			setColor(col)
-			time.sleep(3)
+		displayColor('green', 3)
+		flashColor('yellow', 3)
+		displayColor('red', 3)
 
 def destroy():
 	p_R.stop()
